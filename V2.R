b85f47eea9d1d006b14d59a2f039f3d26209660b
chardensoft <chardensoft@gmail.com>
2021-06-12 13:25:48 -1000

    added excel for elo explanations

--- V2.R
+++ V2.R
@@ -5,3 +5,137 @@ library(car) #for analysis
 library(readxl) #to read specific sheets
 library(corrplot) #for correlation stuff
 
+
+master_var <- master[,-1]
+master_var_clean <- master_var[is.finite(rowSums(master_var)),]
+master_var_reg <- master_var_clean
+
+### Initial Linear Model with all variables
+nbasalary_lm <- lm(MaxElo ~ ., data = master_var_reg)
+summary(nbasalary_lm)
+
+master_var_clean$residuals <- nbasalary_lm$residuals
+master_var_clean$fitted.values <- nbasalary_lm$fitted.values
+
+## Assumption Checking
+
+### No multicollinearity
+bf.vifs <- vif(nbasalary_lasso_lm)
+bf.vifs
+mean(bf.vifs)
+
+### Linearity
+
+#### Residuals vs. Fitted Values plot
+salary.resid.plot <- autoplot(nbasalary_lasso_lm, which = 1, ncol = 1, nrow = 1) +
+  theme_bw() + 
+  theme(aspect.ratio = 1)
+salary.resid.plot
+
+#### Added-Variable plots
+avPlots(nbasalary_lm)
+
+### Normality of residuals and centered at zero
+
+#### Boxplot of residuals
+salary.box <- ggplot(data = master_var_clean, mapping = aes(y = residuals)) +
+  geom_boxplot() +
+  theme(aspect.ratio = 1)
+salary.box
+
+#### Histogram of residuals
+salary.hist <- ggplot(data = master_var_clean, mapping = aes(x = residuals)) +
+  geom_histogram(mapping = aes(y = ..density..), binwidth = 50) +
+  stat_function(fun = dnorm, color = "red", size = 2, 
+                args = list(mean = mean(master_var_clean$residuals), sd = sd(master_var_clean$residuals))) +
+  theme_bw() + 
+  theme(aspect.ratio = 1)
+
+salary.hist
+
+#### QQ plot
+bf.qq.plot <- autoplot(nbasalary_lm, which = 2, ncol = 1, nrow = 1) +
+  theme_bw() + 
+  theme(aspect.ratio = 1)
+bf.qq.plot
+
+#### Shapiro-Wilke test 
+shapiro.test(master_var_clean$residuals)
+
+### The residuals have equal/constant variance across all values of X
+
+salary.resid.plot
+
+### The model describes all observations
+
+#### DFFITS
+master_var_clean$dffits <- dffits(nbasalary_lm)
+
+ggplot(data = master_var_clean) + 
+  geom_point(mapping = aes(x = as.numeric(rownames(master_var_clean)), 
+                           y = abs(dffits))) +
+  theme_bw() +
+  ylab("Absolute Value of DFFITS for Y") +
+  xlab("Observation Number") +
+  geom_hline(mapping = aes(yintercept = 2 * sqrt(length(nbasalary_lm$coefficients) /
+                                                   length(dffits))),
+             color = "red", linetype = "dashed") +
+  scale_x_continuous(limits = c(0, dim(master_var_clean)[1])) +
+  scale_y_continuous(limits = (c(0, max(dffits(nbasalary_lm)) + 0.5))) +
+  theme(aspect.ratio = 1) 
+
+#### Variable Selection
+
+library(bestglm)
+
+#### Organizing variables
+master_var_reg_selection <- master_var_reg[, c(2:dim(master_var_reg)[2], 1)]
+
+##### LASSO
+library(glmnet)
+salary_x <- as.matrix(master_var_reg_selection[, 1:(dim(master_var_reg_selection)[2]-1)])
+salary_y <- master_var_reg_selection[, dim(master_var_reg_selection)[2]]
+
+salary_lasso_cv <- cv.glmnet(x = salary_x,
+                             y = salary_y, 
+                             type.measure = "mse", 
+                             alpha = 1)  
+
+c_lasso <- coef(salary_lasso_cv,s='lambda.1se',exact=TRUE)
+inds_lasso <- which(c!=0)
+variables_lasso <- row.names(c)[inds_lasso]
+variables_lasso <- variables_lasso[-1]
+fmla_lasso <- as.formula(paste("MaxElo ~ ", paste(variables_lasso, collapse= "+")))
+
+nbasalary_lasso_lm <- lm(fmla_lasso, data = master_var_reg )
+summary(nbasalary_lasso_lm)
+
+#### Ridge Regression
+salary_ridge_cv <- cv.glmnet(x = salary_x,
+                             y = salary_y, 
+                             type.measure = "mse", 
+                             alpha = 0)  
+
+c_ridge <- coef(salary_ridge_cv,s='lambda.1se',exact=TRUE)
+inds_ridge <- which(c!=0)
+variables_ridge <- row.names(c)[inds_ridge]
+variables_ridge <- variables_ridge[-1]
+fmla_ridge <- as.formula(paste("MaxElo ~ ", paste(variables_ridge, collapse= "+")))
+
+nbasalary_ridge_lm <- lm(fmla_ridge, data = master_var_reg )
+summary(nbasalary_ridge_lm)
+
+##### Elastic NET
+salary_elastic_net_cv <- cv.glmnet(x = salary_x,
+                                   y = salary_y, 
+                                   type.measure = "mse", 
+                                   alpha = 0.5)
+
+c_en<-coef(salary_elastic_net_cv,s='lambda.1se',exact=TRUE)
+inds_en<-which(c!=0)
+variables_en<-row.names(c)[inds_en]
+variables_en<-variables_en[-1]
+fmla_en <- as.formula(paste("MaxElo ~ ", paste(variables_en, collapse= "+")))
+
+nbasalary_elastic_lm <- lm(fmla_en, data = master_var_reg )
+summary(nbasalary_elastic_lm)
--- traintest.R
+++ traintest.R
@@ -1,186 +1,3 @@
-#   geom_point() +
-#   theme_bw()
-#
-# ggplot(data = train, mapping = aes(y = min_sal, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spMP, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spWS, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spBPM, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spPTS, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spPER, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spVORP, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = min_spUSG, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-#
-# ggplot(data = train, mapping = aes(y = std_sal, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spMP, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spWS, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spBPM, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spPTS, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spPER, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spVORP, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = std_spUSG, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-#
-# ggplot(data = train, mapping = aes(y = sal_C, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = sal_PF, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = sal_PG, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = sal_SF, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = sal_SG, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = sal_Bench, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-# ggplot(data = train, mapping = aes(y = sal_Start, x = MaxElo)) +
-#   geom_point() +
-#   theme_bw()
-##### MultiCollinearity
-corrplot(cor(train), type = "upper")
-#### Variable Selection
-#### Organizing variables
-master_var_reg_selection <- train[, c(2:dim(train)[2], 1)]
-set.seed(seed = 3412)
-##### LASSO
-salary_x <- as.matrix(master_var_reg_selection[, 1:(dim(master_var_reg_selection)[2]-1)])
-salary_y <- master_var_reg_selection[, dim(master_var_reg_selection)[2]]
-salary_lasso_cv <- cv.glmnet(x = salary_x,
-y = salary_y,
-type.measure = "mse",
-alpha = 1)
-c_lasso <- coef(salary_lasso_cv,s='lambda.1se',exact=TRUE)
-inds_lasso <- which(c_lasso!=0)
-variables_lasso <- row.names(c_lasso)[inds_lasso]
-variables_lasso <- variables_lasso[-1]
-fmla_lasso <- as.formula(paste(names(train)[1], " ~ ", paste(variables_lasso, collapse= "+")))
-nbasalary_lasso_lm <- lm(fmla_lasso, data = train)
-summary(nbasalary_lasso_lm)
-#### Ridge Regression
-salary_ridge_cv <- cv.glmnet(x = salary_x,
-y = salary_y,
-type.measure = "mse",
-alpha = 0)
-c_ridge <- coef(salary_ridge_cv,s='lambda.1se',exact=TRUE)
-inds_ridge <- which(c_ridge!=0)
-variables_ridge <- row.names(c_ridge)[inds_ridge]
-variables_ridge <- variables_ridge[-1]
-fmla_ridge <- as.formula(paste(names(train)[1], " ~ ", paste(variables_ridge, collapse= "+")))
-nbasalary_ridge_lm <- lm(fmla_ridge, data = train )
-summary(nbasalary_ridge_lm)
-##### Elastic NET
-salary_elastic_net_cv <- cv.glmnet(x = salary_x,
-y = salary_y,
-type.measure = "mse",
-alpha = 0.5)
-c_en<-coef(salary_elastic_net_cv,s='lambda.1se',exact=TRUE)
-inds_en<-which(c_en!=0)
-variables_en<-row.names(c_en)[inds_en]
-variables_en<-variables_en[-1]
-fmla_en <- as.formula(paste(names(train)[1], " ~ ", paste(variables_en, collapse= "+")))
-nbasalary_elastic_lm <- lm(fmla_en, data = train )
-summary(nbasalary_elastic_lm)
-###New data set with lasso variables
-selected_vars <- master_var_reg_selection[c(which(names(master_var_reg_selection) %in% variables_lasso), 48)]
-selected_plots <- list()
-for (i in 2:length(selected_vars)) {
-selected_plots[[i-1]] <- ggplot(data = selected_vars, mapping = aes_string(y = names(selected_vars)[i],
-x = names(train)[1])) +
-geom_point()
-}
-#### Multicollinearity
-corrplot(cor(selected_vars), type = "upper")
-##Eliminate 5,9,12,13,10,15,3,
-##due to multicollinearity and added back sal_Bench, sal_PG, sal_C, max_sal, tot_sal to take a closer
-selected_vars <- selected_vars[-c(3,5)]
-selected_vars <- cbind(selected_vars[1:16], sal_C = train$sal_C,
-selected_vars[17:21], sal_Bench = train$sal_Bench,
-selected_vars[22])
-corrplot(cor(selected_vars), type = "upper")
-# #
-# # ggplot(data = train, mapping = aes(y = max_sal, x = std_sal)) +
-# #     geom_point() +
-# #     theme_bw()
-# # ggplot(data = train, mapping = aes(y = std_sal, x = MaxElo)) +
-# #   geom_point() +
-# #   theme_bw()
-# # ggplot(data = train, mapping = aes(y = max_sal, x = MaxElo)) +
-# #   geom_point() +
-# #   theme_bw()
-# # ggplot(data = train, mapping = aes(y = sal_Bench, x = MaxElo)) +
-# #   geom_point() +
-# #   theme_bw()
-# # ggplot(data = train, mapping = aes(y = sal_Start, x = MaxElo)) +
-# #   geom_point() +
-# #   theme_bw()
-# #
-# # ##Further remove avg_spPTS, min_spVORP, max_sal, tot_spVORP
-# #
-# # selected_vars <- selected_vars[-c(6, 8, 4, 14)]
-# # corrplot(cor(selected_vars), type = "upper")
-## New Linear model with final variables
-variables <- names(selected_vars)[-c(dim(selected_vars)[2])]
-fmla <- as.formula(paste(names(train)[1], " ~ ", paste(variables, collapse= "+")))
-nbasalary_lm <- lm(fmla, data = train)
-summary(nbasalary_lm)
-## Make Random Forest model with final variables
-rf_model <- randomForest(fmla, data = train)
-summary(rf_model)
-varImpPlot(rf_model)
-plot(rf_model)
-## Testing both:
-### Linear Model:
-lin_results <- data.frame(cbind(master$id[517:638], round(predict(nbasalary_lm, test), 3),
-master$MaxElo[517:638]))
-names(lin_results) <- c("id", "predicted", "actual")
-lin_results$predicted <- as.numeric(lin_results$predicted)
-lin_results$actual <- as.numeric(lin_results$actual)
-lin_results$diff <- lin_results$actual - lin_results$predicted
-lin_perc_wn_100 <- length(which(abs(lin_results$diff) < 100)) / length(lin_results$diff)
-### Random Forest Model
-rf_results <- data.frame(cbind(master$id[517:638], round(predict(rf_model, test), 3),
-master$MaxElo[517:638]))
-names(rf_results) <- c("id", "predicted", "actual")
-rf_results$predicted <- as.numeric(rf_results$predicted)
-rf_results$actual <- as.numeric(rf_results$actual)
-rf_results$diff <- rf_results$actual - rf_results$predicted
-rf_perc_wn_100 <- length(which(abs(rf_results$diff) < 100)) / length(rf_results$diff)
 ##Version 2 of model
 library(tidyverse) #for graphs
 library(ggfortify) #for some fancy graph stuff
@@ -192,27 +9,37 @@ library(glmnet)
 library(gridExtra)
 library(randomForest)
 rm(list = ls())
-name <- "master.csv" #adjust to match the name of your data sheet,
+
+name <- "master.csv" #adjust to match the name of your data sheet, 
 #put the id in column 1, the data you're trying to predict in column 2 and explanatory variables after
+
 master <- read.csv(file = name)
+
 master_clean <- master[is.finite(rowSums(master[,-1])),]
 master_var_reg <- master_clean[,-1]
+
 ## Split into training and testing data
+
 train <- master_var_reg[1:516,]
 test <- master_var_reg[517:638,]
+
 ### Initial Linear Model with all variables
 nbasalary_all_lm <- lm(MaxElo ~ ., data = train)
 summary(nbasalary_all_lm)
+
 # master_var_clean$residuals <- nbasalary_all_lm$residuals
 # master_var_clean$fitted.values <- nbasalary_all_lm$fitted.values
+
 #### Plots
 plots <- list()
 for (i in 2:length(train)) {
-plots[[i-1]] <- ggplot(data = train, mapping = aes_string(y = names(train)[i],
-x = names(train)[1])) +
-geom_point()
+plots[[i-1]] <- ggplot(data = train, mapping = aes_string(y = names(train)[i], 
+                                                                   x = names(train)[1])) +
+  geom_point()
 }
+
 # do.call(grid.arrange, plots)
+
 # ggplot(data = train, mapping = aes(y = tot_sal, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
@@ -237,7 +64,7 @@ geom_point()
 # ggplot(data = train, mapping = aes(y = tot_spUSG, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
-#
+# 
 # ggplot(data = train, mapping = aes(y = avg_sal, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
@@ -262,7 +89,7 @@ geom_point()
 # ggplot(data = train, mapping = aes(y = avg_spUSG, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
-#
+# 
 # ggplot(data = train, mapping = aes(y = max_sal, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
@@ -287,7 +114,7 @@ geom_point()
 # ggplot(data = train, mapping = aes(y = max_spUSG, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
-#
+# 
 # ggplot(data = train, mapping = aes(y = min_sal, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
@@ -312,7 +139,7 @@ geom_point()
 # ggplot(data = train, mapping = aes(y = min_spUSG, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
-#
+# 
 # ggplot(data = train, mapping = aes(y = std_sal, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
@@ -337,7 +164,7 @@ geom_point()
 # ggplot(data = train, mapping = aes(y = std_spUSG, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
-#
+# 
 # ggplot(data = train, mapping = aes(y = sal_C, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
@@ -359,69 +186,93 @@ geom_point()
 # ggplot(data = train, mapping = aes(y = sal_Start, x = MaxElo)) +
 #   geom_point() +
 #   theme_bw()
+
+
 ##### MultiCollinearity
+
 corrplot(cor(train), type = "upper")
+
 #### Variable Selection
+
 #### Organizing variables
 master_var_reg_selection <- train[, c(2:dim(train)[2], 1)]
 set.seed(seed = 3412)
+
 ##### LASSO
 salary_x <- as.matrix(master_var_reg_selection[, 1:(dim(master_var_reg_selection)[2]-1)])
 salary_y <- master_var_reg_selection[, dim(master_var_reg_selection)[2]]
+
 salary_lasso_cv <- cv.glmnet(x = salary_x,
-y = salary_y,
-type.measure = "mse",
-alpha = 1)
+                             y = salary_y, 
+                             type.measure = "mse", 
+                             alpha = 1)  
+
 c_lasso <- coef(salary_lasso_cv,s='lambda.1se',exact=TRUE)
 inds_lasso <- which(c_lasso!=0)
 variables_lasso <- row.names(c_lasso)[inds_lasso]
 variables_lasso <- variables_lasso[-1]
 fmla_lasso <- as.formula(paste(names(train)[1], " ~ ", paste(variables_lasso, collapse= "+")))
+
 nbasalary_lasso_lm <- lm(fmla_lasso, data = train)
 summary(nbasalary_lasso_lm)
+
 #### Ridge Regression
 salary_ridge_cv <- cv.glmnet(x = salary_x,
-y = salary_y,
-type.measure = "mse",
-alpha = 0)
+                             y = salary_y, 
+                             type.measure = "mse", 
+                             alpha = 0)  
+
 c_ridge <- coef(salary_ridge_cv,s='lambda.1se',exact=TRUE)
 inds_ridge <- which(c_ridge!=0)
 variables_ridge <- row.names(c_ridge)[inds_ridge]
 variables_ridge <- variables_ridge[-1]
 fmla_ridge <- as.formula(paste(names(train)[1], " ~ ", paste(variables_ridge, collapse= "+")))
+
 nbasalary_ridge_lm <- lm(fmla_ridge, data = train )
 summary(nbasalary_ridge_lm)
+
 ##### Elastic NET
 salary_elastic_net_cv <- cv.glmnet(x = salary_x,
-y = salary_y,
-type.measure = "mse",
-alpha = 0.5)
+                                   y = salary_y, 
+                                   type.measure = "mse", 
+                                   alpha = 0.5)
+
 c_en<-coef(salary_elastic_net_cv,s='lambda.1se',exact=TRUE)
 inds_en<-which(c_en!=0)
 variables_en<-row.names(c_en)[inds_en]
 variables_en<-variables_en[-1]
 fmla_en <- as.formula(paste(names(train)[1], " ~ ", paste(variables_en, collapse= "+")))
+
 nbasalary_elastic_lm <- lm(fmla_en, data = train )
 summary(nbasalary_elastic_lm)
+
 ###New data set with lasso variables
+
 selected_vars <- master_var_reg_selection[c(which(names(master_var_reg_selection) %in% variables_lasso), 48)]
+
 selected_plots <- list()
 for (i in 2:length(selected_vars)) {
-selected_plots[[i-1]] <- ggplot(data = selected_vars, mapping = aes_string(y = names(selected_vars)[i],
-x = names(train)[1])) +
-geom_point()
+  selected_plots[[i-1]] <- ggplot(data = selected_vars, mapping = aes_string(y = names(selected_vars)[i], 
+                                                                             x = names(train)[1])) +
+    geom_point()
 }
+
 #### Multicollinearity
+
 corrplot(cor(selected_vars), type = "upper")
+
+
 ##Eliminate 5,9,12,13,10,15,3,
 ##due to multicollinearity and added back sal_Bench, sal_PG, sal_C, max_sal, tot_sal to take a closer
+
 selected_vars <- selected_vars[-c(3,5,10)]
 selected_vars <- cbind(tot_sal = train$tot_sal, selected_vars[1:15], max_sal = train$max_sal,
-sal_C = train$sal_C,
-selected_vars[16:20], sal_Bench = train$sal_Bench,
-selected_vars[21])
+                       sal_C = train$sal_C,
+                       selected_vars[16:20], sal_Bench = train$sal_Bench,
+                       selected_vars[21])
+
 corrplot(cor(selected_vars), type = "upper")
-# #
+# # 
 # # ggplot(data = train, mapping = aes(y = max_sal, x = std_sal)) +
 # #     geom_point() +
 # #     theme_bw()
@@ -437,76 +288,46 @@ corrplot(cor(selected_vars), type = "upper")
 # # ggplot(data = train, mapping = aes(y = sal_Start, x = MaxElo)) +
 # #   geom_point() +
 # #   theme_bw()
-# #
+# # 
 # # ##Further remove avg_spPTS, min_spVORP, max_sal, tot_spVORP
-# #
+# # 
 # # selected_vars <- selected_vars[-c(6, 8, 4, 14)]
 # # corrplot(cor(selected_vars), type = "upper")
+
 ## New Linear model with final variables
 variables <- names(selected_vars)[-c(dim(selected_vars)[2])]
 fmla <- as.formula(paste(names(train)[1], " ~ ", paste(variables, collapse= "+")))
+
 nbasalary_lm <- lm(fmla, data = train)
 summary(nbasalary_lm)
+
 ## Make Random Forest model with final variables
 rf_model <- randomForest(fmla, data = train)
 summary(rf_model)
 varImpPlot(rf_model)
 plot(rf_model)
+
 ## Testing both:
 ### Linear Model:
-lin_results <- data.frame(cbind(master$id[517:638], round(predict(nbasalary_lm, test), 3),
-master$MaxElo[517:638]))
+lin_results <- data.frame(cbind(master$id[517:638], round(predict(nbasalary_lm, test), 3), 
+                                master$MaxElo[517:638]))
 names(lin_results) <- c("id", "predicted", "actual")
 lin_results$predicted <- as.numeric(lin_results$predicted)
 lin_results$actual <- as.numeric(lin_results$actual)
 lin_results$diff <- lin_results$actual - lin_results$predicted
 lin_perc_wn_100 <- length(which(abs(lin_results$diff) < 100)) / length(lin_results$diff)
+
+
 ### Random Forest Model
 rf_results <- data.frame(cbind(master$id[517:638], round(predict(rf_model, test), 3),
-master$MaxElo[517:638]))
+                                    master$MaxElo[517:638]))
 names(rf_results) <- c("id", "predicted", "actual")
 rf_results$predicted <- as.numeric(rf_results$predicted)
 rf_results$actual <- as.numeric(rf_results$actual)
 rf_results$diff <- rf_results$actual - rf_results$predicted
 rf_perc_wn_100 <- length(which(abs(rf_results$diff) < 100)) / length(rf_results$diff)
-##Version 2 of model
-library(tidyverse) #for graphs
-library(ggfortify) #for some fancy graph stuff
-library(car) #for analysis
-library(readxl) #to read specific sheets
-library(corrplot) #for correlation stuff
-library(bestglm)
-library(glmnet)
-library(gridExtra)
-library(randomForest)
-rm(list = ls())
-name <- "master.csv" #adjust to match the name of your data sheet,
-master <- read.csv(file = name)
-master_clean <- master[is.finite(rowSums(master[,-1])),]
-master_var_reg <- master_clean[,-1]
-train <- master_var_reg[1:516,]
-test <- master_var_reg[517:638,]
-nbasalary_lm <- lm(MaxElo ~ ., data = train)
-summary(nbasalary_lm)
-## Make Random Forest model with final variables
-rf_model <- randomForest(MaxElo ~ ., data = train)
-summary(rf_model)
-varImpPlot(rf_model)
-plot(rf_model)
-## Testing both:
-### Linear Model:
-lin_results <- data.frame(cbind(master$id[517:638], round(predict(nbasalary_lm, test), 3),
-master$MaxElo[517:638]))
-names(lin_results) <- c("id", "predicted", "actual")
-lin_results$predicted <- as.numeric(lin_results$predicted)
-lin_results$actual <- as.numeric(lin_results$actual)
-lin_results$diff <- lin_results$actual - lin_results$predicted
-lin_perc_wn_100 <- length(which(abs(lin_results$diff) < 100)) / length(lin_results$diff)
-### Random Forest Model
-rf_results <- data.frame(cbind(master$id[517:638], round(predict(rf_model, test), 3),
-master$MaxElo[517:638]))
-names(rf_results) <- c("id", "predicted", "actual")
-rf_results$predicted <- as.numeric(rf_results$predicted)
-rf_results$actual <- as.numeric(rf_results$actual)
-rf_results$diff <- rf_results$actual - rf_results$predicted
-rf_perc_wn_100 <- length(which(abs(rf_results$diff) < 100)) / length(rf_results$diff)
+
+
+
+
+
